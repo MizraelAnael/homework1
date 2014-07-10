c4ed163c1df8daf8f395173eb95c9ebcbb01598b
MizraelAnael <77sefirot77@mail.ru>
2014-07-10 13:09:47 +0400

    Убрал лишние функции

--- Домашнее задание 1/Task 3.cpp
+++ Домашнее задание 1/Task 3.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int getSpaceCount ( char* str )  // Определяет кол-во пробелов
+int getSpaceCount ( char* str )  // Определяет кол-во пробелов вообще
 {
 	int prob = 0;
     while ( *str != '\0' )
@@ -17,63 +17,40 @@ int getSpaceCount ( char* str )  // Определяет кол-во пробелов
     return prob;
 }
 
-bool getDoubleSpace ( char* str ) // Проводит проверку на наличие двойных пробелов
-{             
-	bool i = false;
+int getSpaceCount2 ( char* str )  // Определяет кол-во пробелов, игнорируя двойные пробелы
+{
+	int prob = 0;
     while ( *str != '\0' )
 		{
-			if ( *str == ' ' && *(str+1) == ' ')
+			if (*str == ' ' && *(str - 1) == ' ')
 				{
-					i = true; // тут уже можно завершать функцию. (Как тут завершить функцию, если нам нужно двигать указатель по строке, чтобы цикл шел?)
+					*str++;					
                 }
-            str++;  
-        }
-    return i;
-}
-
-void deleteDoubleSpaces ( char* str1, char* str2 ) // Удаляет лишний пробел
-{
-	while ( *str1 != '\0' )
-		{
-			if ( *str1 == ' ' && *(str1-1) == ' ' )
+			else
 				{
-					str1++;
-                }
-            else 
-                {
-                    *str2 = *str1;
-                    str1++;
-                    str2++;
-                }
+					if (*str == ' ')
+						{
+							prob++;
+							*str++;
+						}
+					else *str++;
+				}
         }
-    *str2 = '\0';
+    return prob;
 }
 
 int getWordCount ( char str[] ) // Считает кол-во слов
 {
-	char str2[200] = "/0";
-	if ( getDoubleSpace (str) == false )
-		{
-			strcpy (str2, str );
-		}
-	else
-		{
-			while ( getDoubleSpace (str) != false )
-				{
-					deleteDoubleSpaces ( str, str2 );
-					strcpy (str, str2 );
-				}			
-		}
-	int slova = getSpaceCount ( str2 );
-    if ( str2[0] == '\0' )
+	int slova = getSpaceCount2 ( str );
+    if ( str[0] == '\0' )
 		{
 			slova--;
 		}
-	if ( str2[0] == ' ') 
+	if ( str[0] == ' ') 
 		{
 			slova--;
         }
-	if ( str2[strlen (str2)-1] == ' ')
+	if ( str[strlen (str)-1] == ' ')
 		{
 			slova--;
         }
@@ -88,5 +65,5 @@ void task3()
 	char str[] = "Предложение для  проверки тестом";
     cout << "Дана строка: " << str << endl;
     cout << "Количество пробелов: " << getSpaceCount ( str ) << endl; 
-    cout << "Количество слов: " << getWordCount ( str ) << endl;
+	cout << "Количество слов: " << getWordCount ( str ) << endl;
 }

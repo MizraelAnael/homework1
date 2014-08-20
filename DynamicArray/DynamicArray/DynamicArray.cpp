7bcac01fe8a1a413751af255aca10fbdd21d1b7c
Denis <dendibakh@gmail.com>
2014-08-20 22:43:45 +0400

    Добавил замечания

--- DynamicArray/DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -1,150 +1,165 @@
 #include "DynamicArray.h"
 
 DynamicArray::DynamicArray()
-{	
-	arr = nullptr;
-	counter = 0;
+{       
+        arr = nullptr;
+        counter = 0;
 }
 
 DynamicArray::~DynamicArray()
-{	
-	clear();
+{       
+        clear();
 }
 
 void DynamicArray::push_back (int element)
 {
-	int *arrTemp = new int[counter+1];
-	for (int i = 0; i < counter; ++i)
-		arrTemp[i] = arr[i];
-	arrTemp[counter] = element;
-	++counter;
-	delete []arr;
-	arr = arrTemp;
+        int *arrTemp = new int[counter+1];
+        for (int i = 0; i < counter; ++i)
+                arrTemp[i] = arr[i];
+        arrTemp[counter] = element;
+        ++counter;
+        delete []arr;
+        arr = arrTemp;
 }
 
 void DynamicArray::pop_back()
 {
-	if (!isEmpty())
-	{
-		counter--;
-		int *arrTemp = new int[counter];		
-		for (int i = 0; i < counter; ++i)
-			arrTemp[i] = arr[i];		
-		delete []arr;
-		arr = arrTemp;
-	}
-	else
-		throw std::runtime_error ("Array is empty. You couldn't delete element");	
+        if (!isEmpty())
+        {
+                counter--;
+                int *arrTemp = new int[counter];                
+                for (int i = 0; i < counter; ++i)
+                        arrTemp[i] = arr[i];            
+                delete []arr;
+                arr = arrTemp;
+        }
+        else
+                throw std::runtime_error ("Array is empty. You couldn't delete element");       
+        /*
+        Можно еще написать вот так:
+        
+        if (isEmpty())
+           throw std::runtime_error ("Array is empty. You couldn't delete element");
+
+        counter--;
+        int *arrTemp = new int[counter];                
+        for (int i = 0; i < counter; ++i)
+                arrTemp[i] = arr[i];            
+        delete []arr;
+        arr = arrTemp;
+
+        Опять же вопрос стиля :). По-моему второй вариант более читабелен.
+        */
 }
 
 int DynamicArray::get_back()
 {
-	if (counter > 0)
-		return arr[counter - 1];
-	else
-		throw std::runtime_error ("Array is empty. You couldn't take element");
+        if (counter > 0)
+                return arr[counter - 1];
+        else
+                throw std::runtime_error ("Array is empty. You couldn't take element");
 }
 
 bool DynamicArray::isEmpty()
 {
-	if (counter == 0)
-		return true;	 
-	return false;
+        if (counter == 0)
+                return true;     
+        return false;
 }
 
 int DynamicArray::size()
 {
-	return counter;
+        return counter;
 }
 
 void DynamicArray::clear()
 {
-	delete []arr;
-	counter = 0;
+        delete []arr;
+        counter = 0;
 }
 
 string DynamicArray::toString()
 {
-	stringstream ss;
-	if (counter == 0)
-	{
-		ss << "Array is empty";
-		return ss.str();
-	}
-	else
-	{
-		for (int i = 0; i < counter; ++i)
-			ss << arr[i] << " ";		
-		return ss.str();
-	}
+        stringstream ss;
+        if (counter == 0)
+        {
+                ss << "Array is empty";
+                return ss.str();
+        }
+        else
+        {
+                for (int i = 0; i < counter; ++i)
+                        ss << arr[i] << " ";            
+                return ss.str();
+        }
 }
 
 void DynamicArray::printArray()
 {
-	cout << toString() << endl;
+        cout << toString() << endl;
 }
 
 DynamicArray::iterator DynamicArray::begin()
 {
-	return arr;
+        return arr;
 }
 
 DynamicArray::iterator DynamicArray::end()
 {
-	return arr+counter;
+        return arr+counter;
 }
 
 DynamicArray::DynamicArray (const DynamicArray& rhs)
 {
-	copyArray (*this, rhs);
+        copyArray (*this, rhs);
 }
 
 DynamicArray DynamicArray::operator= (const DynamicArray& rhs)
 {
-	clear();
-	copyArray (*this, rhs);
-	return *this;
+        clear();
+        copyArray (*this, rhs);
+        return *this;
 }
 
 void DynamicArray::copyArray(DynamicArray& copy, const DynamicArray& origin)
 {
-	copy.arr = new int [origin.counter];
-	copy.counter = 0;
-	for (int i = 0; i < origin.counter; ++i)
-		copy.push_back (origin.arr[i]);
+        copy.arr = new int [origin.counter];
+        copy.counter = 0;
+        for (int i = 0; i < origin.counter; ++i)
+                copy.push_back (origin.arr[i]);
 }
 
 int& DynamicArray::iterator::operator*()
 {
-	return *iter;
+        return *iter;
 }
 
 int* DynamicArray::iterator::operator->()
 {
-	return iter;
+        return iter;
 }
 
 DynamicArray::iterator& DynamicArray::iterator::operator++()
 {
-	++iter;
-	return *this;
+        ++iter;
+        return *this;
 }
 
 DynamicArray::iterator DynamicArray::iterator::operator++(int)
 {
-	iterator temp = *this;
-	++iter;
-	return temp;
+        iterator temp = *this;
+        ++iter;
+        return temp;
 }
 
 bool DynamicArray::iterator::operator ==(const iterator& temp)
 {
-	if (this->iter == temp.iter)
-		return true;
-	return false;
+        if (this->iter == temp.iter)
+                return true;
+        return false;
 }
 
 bool DynamicArray::iterator::operator != (const iterator& temp)
 {
-	return !(*this == temp);
+        return !(*this == temp);
 }
--- DynamicArray/DynamicArray/DynamicArray.h
+++ DynamicArray/DynamicArray/DynamicArray.h
@@ -1,45 +1,54 @@
 #pragma once
 
+// Действительно ли эти включения нужны для компиляции данного заголовочного файла?
 #include <string>
 #include <sstream> 
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
+using namespace std; // Этого не должно быть в заголовочном файле.
 
+// Константные методы нужно пометить с помощью const
 
 class DynamicArray
 {
 private:
-	void copyArray (DynamicArray& origin, const DynamicArray& temp);
+        void copyArray (DynamicArray& origin, const DynamicArray& temp);
 public:
-	DynamicArray();	
-	~DynamicArray();	
-	DynamicArray (const DynamicArray& rhs);
-	DynamicArray operator= (const DynamicArray& rhs);
-	void clear();
-	int size();
-	bool isEmpty();
-	void push_back (int element);
-	void pop_back();
-	int get_back();
-	string toString();
-	void printArray();
-	class iterator
-	{
-	public:
-		iterator(int *iter_ = 0) : iter(iter_) {}		
-		int& operator*(); 		
-		int* operator->();		
-		iterator& operator++();		
-		iterator operator++(int);		
-		bool operator ==(const iterator& temp);		
-		bool operator != (const iterator& temp);		
-	private:
-		int *iter;
-	};
-	iterator begin();
-	iterator end();
+        DynamicArray(); 
+        ~DynamicArray();        
+        DynamicArray (const DynamicArray& rhs);
+        DynamicArray operator= (const DynamicArray& rhs);
+        void clear();
+        int size();
+        bool isEmpty();
+        void push_back (int element);
+        void pop_back();
+        int get_back();
+        string toString();
+        void printArray();
+        class iterator
+        {
+        public:
+                // Данный конструктор можно сделать в виде двух:
+                // iterator(int *iter_) : iter(iter_) {}
+                // iterator() : iter(0) {}
+                // Принципиально эта версия ничем не отличается, но мне больше нравится два разных конструктора.
+                // Для справки: значение по умолчанию (которое указывается с помощью = 0 после аргумента) -
+                // это пережиток языка С. В нем нельзя было перегружать функции, ну и конструкторы конечно :).
+                // Тут кому как нравится - вопрос стиля программирования.
+                iterator(int *iter_ = 0) : iter(iter_) {}               
+                int& operator*();               
+                int* operator->();              
+                iterator& operator++();         
+                iterator operator++(int);               
+                bool operator ==(const iterator& temp);         
+                bool operator != (const iterator& temp);                
+        private:
+                int *iter;
+        };
+        iterator begin();
+        iterator end();
 private:
-	int *arr;
-	int counter;
+        int *arr;
+        int counter;
 };
